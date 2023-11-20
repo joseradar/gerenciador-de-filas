83ef35c22b6b9c880525568a4040db8c73825532
José Neto <jose.radar@hotmail.com>
2023-11-20 13:05:01 -0300

    Teste Readme

--- routes/web.php
+++ routes/web.php
@@ -23,7 +23,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('queue.index');
 });
 
 Route::get('filas', [QueueController::class, 'index'])->name('queue.index');
